c0e4f7d9c594b2b934190b3e3141313bf9c328fb
vtu29699 <vtu29699@veltech.edu.in>
2026-08-04 12:11:01 +0530

    Add method to get day of the week for a date

---
+++ session12/task2/code.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+        return LocalDate.of(year, month, day)
+                        .getDayOfWeek()
+                        .getDisplayName(TextStyle.FULL, Locale.US);
+    }
+}
